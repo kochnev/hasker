999db9feec987e269a2e2741fd9b3ff7bba3fff0
Alexander <lyotchik1@mail.ru>
2018-01-28 01:36:40 +0300

    header in index page

--- Makefile
+++ Makefile
@@ -15,6 +15,18 @@ prod:
 	#psycopg2
 	pip3.6 install psycopg2
 	
+	#postgresql
+	yum install -y postgresql-server postgresql-contrib
+	sudo -u postgres /usr/bin/initdb /var/lib/pgsql/data/
+	sudo -u postgres /usr/bin/pg_ctl start  -D /var/lib/pgsql/data -s -o "-p 5432" -w -t 300
+	
+	#create user and database
+	sudo -u postgres psql -f postgresql/init.sql
+
+        #init django project
+	python3.6 manage.py collectstatic
+	python3.6 manage.py migrate
+
 	#nginx
 	yum -y install nginx
 	cp /hasker/nginx/nginx_hasker.conf /etc/nginx/conf.d/
@@ -25,11 +37,3 @@ prod:
 	yum -y install gcc
 	pip3.6 install uwsgi
 	uwsgi --ini /hasker/uwsgi/uwsgi.ini
-
-	#postgresql
-	yum install -y postgresql-server postgresql-contrib
-	sudo -u postgres /usr/bin/initdb /var/lib/pgsql/data/
-	sudo -u postgres /usr/bin/pg_ctl start  -D /var/lib/pgsql/data -s -o "-p 5432" -w -t 300
-	
-	#create user and database
-	sudo -u postgres psql -f postgresql/init.sql
--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.insert(0, os.path.join(BASE_DIR, 'hasker'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ask',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'hasker/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,3 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'hasker/static/')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'hasker/media/')
+MEDIA_URL = '/media/'
