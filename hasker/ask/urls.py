f4d6ab577814a4a967b93f00af969c66eb214bd1
Alexander <lyotchik1@mail.ru>
2018-01-29 00:23:38 +0300

    signup login logout

--- hasker/ask/urls.py
+++ hasker/ask/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^signup/', views.signup, name='signup'),
+    url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^login/', views.user_login, name='login'),
 ]
--- hasker/ask/views.py
+++ hasker/ask/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserForm
 
 
 # Create your views here.
@@ -8,3 +11,39 @@ def index(request):
         'ask/index.html',
         {},
     )
+
+
+def signup(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        user_form = UserForm()
+    return render(request, 'ask/signup.html',
+                      {'user_form': user_form,})
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.add_message(request, messages.ERROR, 'Пользователь заблокирован!')
+                return redirect('login')
+        else:
+            messages.add_message(request, messages.ERROR, 'Неверно введен логин или пароль!')
+            return redirect('login')
+    else:
+        return render(request, 'ask/login.html', {})
+
