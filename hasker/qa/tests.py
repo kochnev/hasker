17d95aa794c78251c9765ac6878d216aec85bbb4
Alexander <lyotchik1@mail.ru>
2018-02-27 17:22:13 +0300

    add some tests

--- hasker/qa/tests.py
+++ hasker/qa/tests.py
@@ -1,29 +1,47 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.shortcuts import reverse
-from .models import Question, Answer
+from .models import Question
+from .forms import QuestionForm
 
 
 class QATests(TestCase):
     @classmethod
     def setUpTestData(cls):
         user1 = get_user_model().objects.create_user(username='test1',
-                                        email='test1@mail.ru',
-                                        password='test123456789')
-
+                                                     email='test1@mail.ru',
+                                                     password='test123456789')
         user1.save()
-
-
         # create 30 Question
         for i in range(30):
             Question.objects.create(title='Question' + str(i), slug='question' + str(i),
                                     text='text of question' + str(i), author=user1)
 
-
     def test_pagination_question_list(self):
         resp = self.client.get(reverse('index'))
         self.assertEqual(resp.status_code, 200)
         self.assertTrue(len(resp.context['questions']) == 20)
+
+    def test_question_form_empty_title(self):
+        form_data = {'text': 'question text'}
+        form = QuestionForm(form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_question_form_empty_text(self):
+        form_data = {'title': 'question title'}
+        form = QuestionForm(form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_question_form_up_to_three_tags(self):
+        self.client.login(username='test1', password='test123456789')
+        resp = self.client.post(reverse('add_question'), {'title': 'title question',
+                                                          'text': 'text question',
+                                                          'tags': 'books,music,football,swimming'})
+        self.assertNotEqual(resp.context['tag_error'], '')
+
+
+
+
 
 
 
--- hasker/search/tests.py
+++ hasker/search/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.shortcuts import reverse
+from ..qa.models import Question
 
-# Create your tests here.
+
+class SearchTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user1 = get_user_model().objects.create_user(username='test1',
+                                        email='test1@mail.ru',
+                                        password='test123456789')
+        user1.save()
+
+        # create 30 Question
+        for i in range(30):
+            q = Question.objects.create(title='Question' + str(i), slug='question' + str(i),
+                                    text='text of question' + str(i), author=user1)
+            q.set_tags('books,music')
+
+    def test_search_by_text(self):
+        resp = self.client.get('%s?q=Quest' % reverse('search'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn('Quest', resp.context['questions'][0].title, )
+        self.assertTrue(len(resp.context['questions']) == 20)
+
+    def test_search_by_tag(self):
+        resp = self.client.get(reverse('tag_search', args=['books']))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(len(resp.context['questions']) == 20)
